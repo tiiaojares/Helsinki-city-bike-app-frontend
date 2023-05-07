7e38279c295226106d91f1611fbff074dc5844f7
tiia <tiia.ojares@gmail.com>
2023-05-07 12:33:06 +0300

    frontend gets stations from the backend

--- helsinki-city-bike/src/App.js
+++ helsinki-city-bike/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import  { Logo }  from './images/logo';
 import { FilterComponent } from './bicycleStation/filterComponent';
 import { CardComponent } from './cards/card';
+import stationService from './services/stations';
 
 
 const App = () => {
   const [stations, setStations] = useState([]);
   const [filterInput, setFilterInput] = useState("");
+
+  //get stations from the backend
+  useEffect(() => {
+    stationService
+      .getAll()
+      .then(response => {
+        console.log("response: ", response);
+        setStations(response);
+      })
+  }, []);
 
 
   return (
@@ -23,9 +34,15 @@ const App = () => {
       </div>
     </nav>
     <div className="container">
-      <div className="row" >
-        <CardComponent pictureUrl={"https://cdn.pixabay.com/photo/2017/08/17/17/44/the-little-yellow-car-2652215_960_720.jpg"} text={"Bicycle stations"} />
-        <CardComponent pictureUrl={"https://cdn.pixabay.com/photo/2014/12/02/03/16/winding-road-553481__340.jpg"} text={"Journeys"} />
+      <div >
+        <div className="row">
+          <CardComponent 
+            pictureUrl={"https://cdn.pixabay.com/photo/2017/08/17/17/44/the-little-yellow-car-2652215_960_720.jpg"} 
+            text={"Bicycle stations"} />        
+          <CardComponent 
+            pictureUrl={"https://cdn.pixabay.com/photo/2014/12/02/03/16/winding-road-553481__340.jpg"} 
+            text={"Journeys"} />
+        </div>
       </div>
     </div>
     </div>
