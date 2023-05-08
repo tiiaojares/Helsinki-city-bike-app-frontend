fcf7661b83ceeda5ad21eab1e532e332aecf92b4
tiia <tiia.ojares@gmail.com>
2023-05-08 10:25:49 +0300

    proxy localhost:3001 added

--- helsinki-city-bike/src/main/MainPage.js
+++ helsinki-city-bike/src/main/MainPage.js
@@ -8,11 +8,11 @@ const MainPage = () => {
         <div className="row">
         <CardComponent 
           pictureUrl={"https://cdn.pixabay.com/photo/2017/08/17/17/44/the-little-yellow-car-2652215_960_720.jpg"} 
-          text={"Bicycle stations"} 
+          text={"Kaupunkipyöräasemat"} 
           navigateTo={"/stations"} />        
         <CardComponent 
           pictureUrl={"https://cdn.pixabay.com/photo/2014/12/02/03/16/winding-road-553481__340.jpg"} 
-          text={"Journeys"} 
+          text={"Matkat"} 
           navigateTo={"/journeys"} />
       </div>
     )
