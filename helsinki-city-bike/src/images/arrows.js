9e9107d6c916f7a7dbb16b22c84d07bf62762a7a
tiia <tiia.ojares@gmail.com>
2023-05-15 13:01:59 +0300

    found journeys pagination works

--- helsinki-city-bike/src/images/arrows.js
+++ helsinki-city-bike/src/images/arrows.js
@@ -3,18 +3,34 @@
 
 const ArrowNext = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
-        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
         </svg>
     )
 }
 
 const ArrowPrevious = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
-        <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
+            <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
         </svg>
     )
 }
 
-export { ArrowNext, ArrowPrevious }
+const ArrowUp = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+</svg>
+    )
+}
+
+const ArrowDown = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
+</svg>
+    )
+}
+
+export { ArrowNext, ArrowPrevious, ArrowUp, ArrowDown }
--- helsinki-city-bike/src/journeys/findJourneys.js
+++ helsinki-city-bike/src/journeys/findJourneys.js
@@ -3,11 +3,14 @@ import { SelectStation } from './selectStation'
 import { useState, useEffect } from 'react';
 import journeyService from '../services/journeys';
 import './journeys.css'
+import { ArrowNext, ArrowPrevious, ArrowUp, ArrowDown } from '../images/arrows';
 
 const JourneyData = ({selectedDepartureStation, selectedReturnStation}) => {
     const [foundJourneys, setFoundJourneys] = useState(null);
     const [sortType, setSortType] = useState("")
     const [sortTypeUpward, setSortTypeUpward] = useState(true);
+    const [maxIndexToShow, setMaxIndexToShow] = useState(15);
+    const [minIndexToShow, setMinIndexToShow] = useState(1);
 
     useEffect(() => {
         journeyService
@@ -33,15 +36,18 @@ const JourneyData = ({selectedDepartureStation, selectedReturnStation}) => {
         }
     }
 
+    const from = minIndexToShow - 1;
+    const to = maxIndexToShow - 1;
+
     function showJourneys() {
         if(foundJourneys) {
-            const tableData = foundJourneys.map( j => 
+            const journeysToShow = foundJourneys.slice(from, to)
+            const tableData = journeysToShow.map( j => 
                 <tr key={j._id}>
                     <td> {j.distance / 1000} </td>
                     <td> {(j.duration / 60).toFixed(1)} </td>
                 </tr>)
-         
-            
+             
             return tableData;
         }
     }
@@ -49,13 +55,34 @@ const JourneyData = ({selectedDepartureStation, selectedReturnStation}) => {
     function changeSortType(type) {
         if (sortType === type) {
             setSortTypeUpward(!sortTypeUpward);
-            console.log("sort type: ", type)
         } else {
             setSortTypeUpward(true);
             setSortType(type);
-            console.log("sort type: ", type)
         }
+    }
 
+    
+    function next() {
+        if (maxIndexToShow + 15 > foundJourneys.length){
+            setMinIndexToShow(minIndexToShow + 15)
+            setMaxIndexToShow(foundJourneys.length);
+        } else {
+            setMinIndexToShow(minIndexToShow + 15)
+            setMaxIndexToShow(maxIndexToShow + 15)
+        }
+    }
+
+    function previous() {
+        if (maxIndexToShow - minIndexToShow < 15){
+            setMaxIndexToShow(minIndexToShow - 1);
+            setMinIndexToShow(minIndexToShow-15)
+        } else if(minIndexToShow - 15 <= 0) {
+            setMinIndexToShow(1);
+            setMaxIndexToShow(15);    
+        } else {
+            setMinIndexToShow(minIndexToShow - 15);
+            setMaxIndexToShow(maxIndexToShow - 15);  
+        }
     }
 
     if(foundJourneys) {
@@ -63,21 +90,47 @@ const JourneyData = ({selectedDepartureStation, selectedReturnStation}) => {
             return <div> Lähtö- ja paluuaseman välillä ei tehtyjä matkoja</div>
         } else {
             return (
-            <div className="table-responsive tableView"> Matkoja: {foundJourneys.length} 
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th onClick={() => changeSortType("distance")}> Matkan pituus (km) </th>
-                            <th onClick={() => changeSortType("duration")}> Matkan kesto (min) </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {showJourneys()}
-                    </tbody>
-                </table>
+            <div>
+                {foundJourneys.length <= 15 && <span> Matkoja löytyi: {foundJourneys.length}</span>}
+                {foundJourneys.length > 15 &&
+                    <div aria-label="Page navigation example">
+                        { maxIndexToShow > 15 && <span className="arrow" onClick={() => previous()}> <ArrowPrevious /> </span> }
+                        <span className="stationPagination"> Matkat {minIndexToShow}-{maxIndexToShow} / {foundJourneys.length}</span>
+                        { maxIndexToShow < foundJourneys.length && <span className="arrow" onClick={() => next()}> <ArrowNext /> </span> }
+                    </div>
+                }
+
+
+                <div className="table-responsive tableView"> 
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th 
+                                className={sortType === "distance" ? "selectedUpward" : "sort"} 
+                                onClick={() => changeSortType("distance")}> 
+                                    Matkan pituus (km) 
+                                    { sortType === "distance" && sortTypeUpward === false && <ArrowDown /> }
+                                    { sortType === "distance" && sortTypeUpward === true && <ArrowUp /> }
+                                </th>
+
+                                <th 
+                                className={sortType === "duration" ? "selectedUpward" : "sort"} 
+                                onClick={() => changeSortType("duration")}>
+                                    Matkan kesto (min) 
+                                    { sortType === "duration" && sortTypeUpward === false && <ArrowDown /> }
+                                    { sortType === "duration" && sortTypeUpward === true && <ArrowUp /> }
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {showJourneys()}
+                        </tbody>
+                    </table>
+                </div>
             </div>
             )
         } 
+    
     } else {
         return (
             <div>
@@ -85,8 +138,7 @@ const JourneyData = ({selectedDepartureStation, selectedReturnStation}) => {
                 <span className="loadingText">Loading...</span>
             </div>
         )
-    }
-    
+    } 
 
 }
 
@@ -108,7 +160,20 @@ const FindJourneys = () => {
 
     return (
     <div>
-        {selectedDepartureStation && selectedReturnStation ? <h2> { selectedDepartureStation.Nimi } - { selectedReturnStation.Nimi } </h2> : <h2> Hae matkoja: </h2>}
+        {selectedDepartureStation && selectedReturnStation ? 
+        <div className="headerSection">
+            <h2> 
+                { selectedDepartureStation.Nimi } - { selectedReturnStation.Nimi } 
+            </h2> 
+            <button 
+                className="btn btn-dark btn-sm newSearchButton"
+                onClick={() => refresh()}> 
+                Uusi haku
+            </button>
+        
+        </div>: 
+        <h2> Hae matkoja: </h2>}
+
         <div className="picture">
             <img className="journeysViewPicture" src="https://cdn.pixabay.com/photo/2014/12/02/03/16/winding-road-553481__340.jpg" />
         </div>
@@ -152,11 +217,7 @@ const FindJourneys = () => {
                             selectedReturnStation={selectedReturnStation} 
                             />
                             <div >
-                                <button 
-                            className="btn btn-dark btn-sm newSearchButton"
-                            onClick={() => refresh()}> 
-                            Uusi haku
-                            </button>
+                      
                             </div>
                         </div> :
                         <div/>
